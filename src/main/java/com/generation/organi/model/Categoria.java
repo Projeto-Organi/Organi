e7c17ec88cbd12f19df847bb81471b5a0c5eb584
Taisluiza <taissalissousa@gmail.com>
2023-07-04 13:43:01 -0300

    Task 5 - Atualizacao(criacao produtoController e relacao de tabelas - CRUD

--- src/main/java/com/generation/organi/model/Categoria.java
+++ src/main/java/com/generation/organi/model/Categoria.java
@@ -1,9 +1,16 @@
 package com.generation.organi.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -23,6 +30,10 @@ public class Categoria {
 	@NotBlank(message = "Campo obrigatório")
 	@Size(min = 15, max = 255, message = "Descrição da categoria deve conter entre 15 a 255 caracteres")
 	private String descricao;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade= CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
 
 	
 	public Long getId() {
@@ -48,6 +59,16 @@ public class Categoria {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+	
+	
 	
 	
 
